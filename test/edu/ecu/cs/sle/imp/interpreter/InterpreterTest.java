5b52c1ffc6f8c8526026b1c1ee83d13d18499bf9
aryan8 <azimzadeha14@students.ecu.edu>
2015-01-28 20:59:12 -0500

    Committing files for Homework1

--- test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
+++ test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
@@ -21,4 +21,34 @@ public class InterpreterTest {
 		IntegerValue iv = interpreter.interpret("1*2");
 		assertEquals("1*2", 2, iv.getIntValue().intValue());
 	}	
+	@Test
+	public void testMinus() {
+		IntegerValue iv = interpreter.interpret("4-2");
+		assertEquals("4-2", 2, iv.getIntValue().intValue());
+	}	
+	@Test
+	public void testPlus() {
+		IntegerValue iv = interpreter.interpret("2+2");
+		assertEquals("2+2", 4, iv.getIntValue().intValue());
+	}	
+	@Test
+	public void testDiv() {
+		IntegerValue iv = interpreter.interpret("2/2");
+		assertEquals("2/2", 1, iv.getIntValue().intValue());
+	}	
+	@Test
+	public void testUnaryMinus() {
+		IntegerValue iv = interpreter.interpret("-2");
+		assertEquals("-1*2", -2, iv.getIntValue().intValue());
+	}	
+	@Test
+	public void testLiteralInt() {
+		IntegerValue iv = interpreter.interpret("1*2");
+		assertEquals("1*2", 1*2, iv.getIntValue().intValue());
+	}	
+	@Test
+	public void testLiteralExp() {
+		IntegerValue iv = interpreter.interpret("2");
+		assertEquals("2", 2, iv.getIntValue().intValue());
+	}
 }
